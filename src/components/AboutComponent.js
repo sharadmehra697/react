dc457a64ed1adb945b9fb6fec205b56f2d5af74b
Sharad Mehra <sharadmehra697@gmail.com>
2020-06-13 01:28:38 +0530

    updated assignment 2

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import RenderLeader from './RenderLeader';
 
 function About(props) {
 
+    function RenderLeader({leader}){
+       return(
+        <div className="col-12 mt-5">
+            <Media tag="li">
+              <Media left middle>
+                  <Media object src={leader.image} alt={leader.name} />
+              </Media>
+              <Media body className="ml-5">
+                <Media heading>{leader.name}</Media>
+                <p>{leader.designation}</p>
+                <p>{leader.description}</p>
+              </Media>
+            </Media>
+          </div>
+              );
+
+    }
+
     const leaders = props.leaders.map((leader) => {
         return (
-            <RenderLeader leaderId={props.leaders.filter((leaderF) => leaderF.id === leader.id)[0]}/>
+            <RenderLeader leader={leader}/>
         );
     });
 
--- src/components/RenderLeader.js
+++ src/components/RenderLeader.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import { Media } from 'reactstrap';
+
 
 
 function RenderLeader(props){
 
-	return(
-		<div className="col-12 mt-5">
-            <Media tag="li">
-              <Media left middle>
-                  <Media object src={props.leaderId.image} alt={props.leaderId.name} />
-              </Media>
-              <Media body className="ml-5">
-                <Media heading>{props.leaderId.name}</Media>
-                <p>{props.leaderId.designation}</p>
-                <p>{props.leaderId.description}</p>
-              </Media>
-            </Media>
-          </div>
-              );
+	
 }
 
 export default RenderLeader;
